dc9b4c462143f1bb736742dfb928af94d347ef9b
Jennifer Liang <jliang24@bu.edu>
2017-09-16 16:58:01 -0400

    Added class for flights

---
+++ flights.py
@@ -0,0 +1,19 @@
+import json
+class Flights(object):
+
+    origin = "null"
+    destination = "null"
+    departure_date = "null"
+    price = "0.0"
+    airline = "null"
+
+    def __init__(self, jsonflightinfo, flight_index):
+        flight_data = json.loads(jsonflightinfo)
+        origin = flight_data["origin"]
+        destination = flight_data["results"][flight_index]["destination"]
+        departure_date = flight_data["departure_date"]
+        price = flight_data["results"]["price"]
+        airline = flight_data["results"][flight_index]["airline"]
+
+
+
