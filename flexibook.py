650a97f88d28cd605b3d1653c172a3f4f0e4361e
costaa17 <CostaA17@Anas-MacBook-Pro.local>
2017-09-16 19:11:02 -0400

    changes to flexibook

--- flexibook.py
+++ flexibook.py
@@ -1,1 +1,16 @@
-#test
+import requests
+import json
+# requires python requests - to install -> pip install requests
+APIKEY = "GP0KwjT7Gkv5ea6wCWuIwonyUKZOVBKN"
+
+# given origin and destination location objects -> returns array of flights
+def getFlights(origin, destination):
+    r = requests.get('http://api.sandbox.amadeus.com/v1.2/flights/extensive-search?apikey=GP0KwjT7Gkv5ea6wCWuIwonyUKZOVBKN&origin=FRA&destination=LON&departure_date=2017-09-17&one-way=true&HTTP/1.1')
+    
+    
+    print(r.text)
+    
+    
+getFlights("dfbhj", "aejhf")
+
+    
