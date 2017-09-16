12b926290c277e49ad96685da8ea586479d28944
Jennifer Liang <jliang24@bu.edu>
2017-09-16 18:25:23 -0400

    Modified indexes for flight data

--- flights.py
+++ flights.py
@@ -3,17 +3,21 @@ class Flights(object):
 
     origin = "null"
     destination = "null"
-    departure_date = "null"
+    departs_at = "0000-00-00T00:00"
+    arrives_at = "0000-00-00T00:00"
     price = "0.0"
     airline = "null"
 
     def __init__(self, jsonflightinfo, flight_index):
         flight_data = json.loads(jsonflightinfo)
-        origin = flight_data["origin"]
-        destination = flight_data["results"][flight_index]["destination"]
-        departure_date = flight_data["departure_date"]
-        price = flight_data["results"]["price"]
-        airline = flight_data["results"][flight_index]["airline"]
+        outbound_flight_index = flight_data["results"][flight_index][0]["itineraries"][0]["outbound"]["flights"][0]
+        outbound_fare_index = flight_data["results"][flight_index][1]["fare"]
+        origin = outbound_flight_index["origin"]["airport"]
+        destination = outbound_flight_index[1]["destination"]["airport"]
+        departs_at = outbound_flight_index[0]["origin"]["departs_at"]
+        arrives_at = outbound_flight_index[0]["origin"]["arrives_at"]
+        airline = outbound_flight_index[0]["origin"]["airport"]
+        price = outbound_fare_index["total_price"]
 
 
 
