a45b79199749e5b3327dca343b0122ab66c02f32
Jennifer Liang <jliang24@bu.edu>
2017-09-16 16:40:44 -0400

    Added class for locations

---
+++ locations.py
@@ -0,0 +1,11 @@
+class Location(object):
+    place = "null"
+    date = "0000-00-00"
+    time = "null"
+
+    def __init__(self, place, date, time):
+        place = place
+        date = date
+        time = time
+
+
