4763746ab3e0caadf7a97f9c0205795290ab5d11
Jennifer Liang <jliang24@bu.edu>
2017-09-16 22:48:43 -0400

    minor updates

--- flights.py
+++ flights.py
@@ -11,13 +11,12 @@ class Flights(object):
     def __init__(self, jsonflightinfo, flight_index):
         flight_data = json.loads(jsonflightinfo)
         outbound_flight_index = flight_data["results"][flight_index]["itineraries"][0]["outbound"]["flights"][0]
-        outbound_fare_index = flight_data["results"][flight_index]["fare"]
         self.origin = outbound_flight_index["origin"]["airport"]
         self.destination = outbound_flight_index["destination"]["airport"]
         self.departs_at = outbound_flight_index["departs_at"]
         self.arrives_at = outbound_flight_index["arrives_at"]
         self.airline = outbound_flight_index["operating_airline"]
-        #price = outbound_fare_index["total_price"]
+        self.price = flight_data["results"][flight_index]["fare"]["price_per_adult"]["total_fare"]
 
 
 
--- locations.py
+++ locations.py
@@ -2,6 +2,8 @@ class Location(object):
     place = "null"
     date = "0000-00-00"
     time = "null"
+    time_earlier = False
+    time_later = False
 
     def __init__(self, place, date, time):
         place = place
