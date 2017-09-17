8599876cf26f8e6ab2626b2b3eca44932642a059
Costaa17 <toiavalle@gmail.com>
2017-09-16 20:26:29 -0400

    fixed json parsing

--- flights.py
+++ flights.py
@@ -10,14 +10,14 @@ class Flights(object):
 
     def __init__(self, jsonflightinfo, flight_index):
         flight_data = json.loads(jsonflightinfo)
-        outbound_flight_index = flight_data["results"][flight_index][0]["itineraries"][0]["outbound"]["flights"][0]
-        outbound_fare_index = flight_data["results"][flight_index][1]["fare"]
-        origin = outbound_flight_index["origin"]["airport"]
-        destination = outbound_flight_index[1]["destination"]["airport"]
-        departs_at = outbound_flight_index[0]["origin"]["departs_at"]
-        arrives_at = outbound_flight_index[0]["origin"]["arrives_at"]
-        airline = outbound_flight_index[0]["origin"]["airport"]
-        price = outbound_fare_index["total_price"]
+        outbound_flight_index = flight_data["results"][flight_index]["itineraries"][0]["outbound"]["flights"][0]
+        outbound_fare_index = flight_data["results"][flight_index]["fare"]
+        self.origin = outbound_flight_index["origin"]["airport"]
+        self.destination = outbound_flight_index["destination"]["airport"]
+        self.departs_at = outbound_flight_index["departs_at"]
+        self.arrives_at = outbound_flight_index["arrives_at"]
+        self.airline = outbound_flight_index["operating_airline"]
+        #price = outbound_fare_index["total_price"]
 
 
 
